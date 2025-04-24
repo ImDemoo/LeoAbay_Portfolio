b3c88d92c0e881d3873af760f7844c71f0d14367
ImDemoo <luwi.abay95@gmail.com>
2025-04-24 13:54:56 +0800

    Move routing directory to the root of the project

--- magic-portfolio-for-next-js/src/app/resources/content.js
+++ magic-portfolio-for-next-js/src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Full Stack Developer",
-  avatar: "/images/avatar.jpg",
+  avatar: "/images/avatar.JPG",
   location: "Asia/Manila", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Filipino"], // optional: Leave the array empty if you don't want to display languages
 };
